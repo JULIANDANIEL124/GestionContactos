d3a0cf40bb1df5c949c4e888919ce6d2b0a5fd0b
JULIANDANIEL124 <165912447+JULIANDANIEL124@users.noreply.github.com>
2024-11-20 07:19:15 -0300

    NUevo tp
    dasdas

---
+++ GestionContactos/GestionContactos/Contacto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionContactos
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string DNI { get; set; }
+    }
+}
